911c5ac336a043622bbee0ea11f814db34eb3338
ravij1904 <ravijeeru1904@gmail.com>
2022-05-19 23:04:33 +0100

    Implementation for retrieving and displaying all the data from CockTailDB URL.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
+import React, {useState, useEffect} from 'react';
 import './style.css';
+import CockTailDetails from './component/CockTailDetails';
 
 function App() {
+  const [letter, setLetter] = useState("")
+  const[cockTailsearch, setCockTailSearch] = useState([])
+
+  // rendering the cocktail details from the cocktailDB URL
+  useEffect(() => {
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=` + letter
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setCockTailSearch(data))
+  }, [letter])
+
   return (
-    <div className='main-container'> 
-     <form>
-       {/* Search bar to enter input */}
+    <>
+    <div className='main-container'>
+      <form>
+        {/* Search bar to enter input */}
         <div className='search-container'>
-          <input type='search' className='search-field'/>          
+          <input type='search' className='search-field' />
         </div>
-       </form> 
+      </form>
     </div>
+    {cockTailsearch?.drinks?.map((cockTail, i) =>{
+      return <CockTailDetails key={i} cockTail= {cockTail} />                    
+  } )}  
+  </> 
   );
 }
 
